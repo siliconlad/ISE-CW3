e501838f104182e57892c131e200dacd7078260a
George Crawford <gcrawf@protonmail.com>
2019-11-16 01:03:56 +0000

    Add basic unit tests to TestLocation.java

--- tests/uk/ac/ed/bikerental/TestLocation.java
+++ tests/uk/ac/ed/bikerental/TestLocation.java
@@ -4,10 +4,69 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
 class TestLocation {
+    String address;
+    String postcode;
+
     @BeforeEach
     void setUp() throws Exception {
-        // TODO: setup some resources before each test
+        address = "10 Downing Street";
     }
-    
-    // TODO: put some tests here
+
+
+    // Testing storage of address/post-code in the class
+    @Test
+    public void detailsShouldBeStored() {
+        postcode = "SW1A2AA";
+        Location loc = new Location(postcode, address);
+
+        assertEquals(address, loc.getAddress());
+        assertEquals(postcode, loc.getPostcode());
+    }
+
+    // Testing input validation for postcodes
+    @Test 
+    public void shortPostcodesShouldFail() {
+        postcode = "SW1A";
+
+        assertThrows(AssertionError.class, () -> new Location(postcode, address));
+    }
+
+    // Testing positive isNearTo scenario
+    @Test
+    public void similarPostcodesShouldBeNear() {
+        postcode = "SW1A2AA";
+
+        String otherAddress = "Palace of Westminster";
+        String otherPostcode = "SW1A0AA";
+
+        Location loc1 = new Location(postcode, address);
+        Location loc2 = new Location(otherPostcode, otherAddress);
+
+        assertTrue(loc1.isNearTo(loc2));
+    }
+
+    // Testing case sensitivity of postcode checks
+    @Test
+    public void checksShouldNotBeCaseSensitive() {
+        postcode = "SW1A2AA";
+
+        Location loc1 = new Location(postcode, address);
+        Location loc2 = new Location(postcode.toLowerCase(), address);
+
+        assertTrue(loc1.isNearTo(loc2));
+    }
+
+    // Testing negative isNearTo scenario
+    @Test
+    public void diffPostcodesShouldNotBeNear() {
+        postcode = "SW1A2AA";
+
+        String otherAddress = "Scottish Parliament Building";
+        String otherPostcode = "EH991SP";
+
+        Location loc1 = new Location(postcode, address);
+        Location loc2 = new Location(otherPostcode, otherAddress);
+
+        assertFalse(loc1.isNearTo(loc2));
+    }
 }
