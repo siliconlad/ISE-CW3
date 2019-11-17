3cb1c464d0e17f2dbd42fc356eae6fc2515c1f55
gcrawf <29676221+gcrawf@users.noreply.github.com>
2019-11-17 14:55:47 +0000

    Refactor methods in TestLocation to match TestDateRange and reduce unnecessary code

--- tests/uk/ac/ed/bikerental/TestLocation.java
+++ tests/uk/ac/ed/bikerental/TestLocation.java
@@ -4,39 +4,24 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
 class TestLocation {
-    String address;
-    String postcode;
+    Location loc1, loc2, loc3;
 
     @BeforeEach
     void setUp() throws Exception {
-        address = "10 Downing Street";
+        this.loc1 = new Location("SW1A2AA", "10 Downing Street");
+        this.loc2 = new Location("SW1A0AA", "Palace of Westminster");
+        this.loc3 = new Location("EH991SP", "Scottish Parliament Building");
     }
 
     // Testing positive isNearTo scenario
     @Test
     public void testIsNearToTrue() {
-        postcode = "SW1A2AA";
-
-        String otherAddress = "Palace of Westminster";
-        String otherPostcode = "SW1A0AA";
-
-        Location loc1 = new Location(postcode, address);
-        Location loc2 = new Location(otherPostcode, otherAddress);
-
         assertTrue(loc1.isNearTo(loc2));
     }
 
     // Testing negative isNearTo scenario
     @Test
     public void testIsNearToFalse() {
-        postcode = "SW1A2AA";
-
-        String otherAddress = "Scottish Parliament Building";
-        String otherPostcode = "EH991SP";
-
-        Location loc1 = new Location(postcode, address);
-        Location loc2 = new Location(otherPostcode, otherAddress);
-
-        assertFalse(loc1.isNearTo(loc2));
+        assertFalse(loc1.isNearTo(loc3));
     }
 }
