4b510fb02006da6c04984d73a734d15a5b2d1252
Siliconlad <38025909+Siliconlad@users.noreply.github.com>
2019-11-20 21:25:33 +0000

    Implement bike class

--- src/uk/ac/ed/bikerental/Bike.java
+++ src/uk/ac/ed/bikerental/Bike.java
@@ -1,9 +1,44 @@
 package uk.ac.ed.bikerental;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
 public class Bike {
+    BikeStatus bikeStatus;
+    BikeType bikeType;
+    ArrayList<Booking> bookings;
+    BigDecimal dailyRentalPrice;
+    Provider provider = null;
+
+    public void Bike(BikeType bikeType, BigDecimal rentalPrice) {
+        this.bikeType = bikeType;
+        this.bikeStatus = BikeStatus.Available;
+        this.dailyRentalPrice = rentalPrice;
+        this.bookings = new ArrayList<Booking>();
+    }
+
     public BikeType getType() {
-        // TODO: Implement Bike.getType
-        assert false;
-        return null;
+        return this.bikeType;
+    }
+
+    public void addProvider(Provider provider) {
+        // Check for invalid state
+        assert this.provider == null;
+
+        this.provider = provider;
+    }
+
+    public void updateStatus(BikeStatus newStatus) {
+        // Check for invalid state
+        assert newStatus != this.bikeStatus;
+
+        this.bikeStatus = newStatus;
+    }
+
+    public void addBooking(Booking booking) {
+        // Check for invalid state
+        assert !this.bookings.contains(booking);
+
+        this.bookings.add(booking);
     }
 }
