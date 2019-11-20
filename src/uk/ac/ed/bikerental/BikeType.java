6f2478ffc57edcad6dbb3e73c909df3622283d43
Siliconlad <38025909+Siliconlad@users.noreply.github.com>
2019-11-20 19:38:36 +0000

    Implement bike type class

--- src/uk/ac/ed/bikerental/BikeType.java
+++ src/uk/ac/ed/bikerental/BikeType.java
@@ -4,9 +4,19 @@ import java.math.BigDecimal;
 import java.util.Objects;
 
 public class BikeType {
+    String name;
+    BigDecimal replacementValue;
+
+    public void BikeType(String name, BigDecimal replacementValue) {
+        this.name = name;
+        this.replacementValue = replacementValue;
+    }
+
     public BigDecimal getReplacementValue() {
-        // TODO: Implement Bike.getReplacementValue
-        assert false;
-        return null;
+        return this.replacementValue;
+    }
+
+    public String getName() {
+        return this.name;
     }
 }
