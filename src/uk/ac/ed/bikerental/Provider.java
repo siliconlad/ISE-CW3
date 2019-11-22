309c6a389c836b064ff89a743b537fcd892dff41
Siliconlad <38025909+Siliconlad@users.noreply.github.com>
2019-11-22 19:40:23 +0000

    Implement functions for the Provider class

--- src/uk/ac/ed/bikerental/Provider.java
+++ src/uk/ac/ed/bikerental/Provider.java
@@ -11,20 +11,40 @@ public class Provider {
     private HashSet<Provider> partners;
     private Collection<Bike> bikes;
 
-    public Provider(String name, Location location, BigDecimal depositRate) {}
+    public Provider(String name, Location location, BigDecimal depositRate) {
+        this.name = name;
+        this.location = location;
+        this.depositRate = depositRate;
+        this.partners = new HashSet<Provider>();
+    }
 
-    public Quote createQuote(Query query) {}
+    public Quote createQuote(Query query) {
+        // TODO: need bookingList
+        assert(false);
+    }
 
-    public String getName() {}
+    public String getName() {
+        return this.name;
+    }
 
-    public Location getLocation() {}
+    public Location getLocation() {
+        return this.location;
+    }
 
-    public BigDecimal getDepositRate() {}
+    public BigDecimal getDepositRate() {
+        return this.depositRate;
+    }
 
-    public void addPartner() {}
+    public void addPartner(Provider newPartner) {
+        assert !this.partners.contains(newPartner);
+
+        this.partners.add(newPartner);
+    }
 
     public Boolean isPartner(Provider partner) {
+        return this.partners.contains(partner);
     }
+
     public void addBikeToStock(Bike bike) {
         assert !this.bikes.contains(bike);
 
