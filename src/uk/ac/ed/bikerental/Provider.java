8795e6b8965a178dba60e3d56f751af7978dcffc
Siliconlad <38025909+Siliconlad@users.noreply.github.com>
2019-11-22 19:39:50 +0000

    Add collection of bikes to Provider class with adder method

--- src/uk/ac/ed/bikerental/Provider.java
+++ src/uk/ac/ed/bikerental/Provider.java
@@ -2,12 +2,14 @@ package uk.ac.ed.bikerental;
 
 import java.math.BigDecimal;
 import java.util.HashSet;
+import java.util.Collection;
 
 public class Provider {
     private String name;
     private Location location;
     private BigDecimal depositRate;
     private HashSet<Provider> partners;
+    private Collection<Bike> bikes;
 
     public Provider(String name, Location location, BigDecimal depositRate) {}
 
@@ -22,6 +24,10 @@ public class Provider {
     public void addPartner() {}
 
     public Boolean isPartner(Provider partner) {
+    }
+    public void addBikeToStock(Bike bike) {
+        assert !this.bikes.contains(bike);
 
+        this.bikes.add(bike);
     }
 }
