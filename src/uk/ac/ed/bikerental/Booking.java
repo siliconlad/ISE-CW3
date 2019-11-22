3ef6555ee664ec881e2037f6b52df5a581721524
gcrawf <29676221+gcrawf@users.noreply.github.com>
2019-11-22 21:21:28 +0000

    Add getter for provider to Booking class

--- src/uk/ac/ed/bikerental/Booking.java
+++ src/uk/ac/ed/bikerental/Booking.java
@@ -44,6 +44,10 @@ public class Booking {
         return this.bikes;
     }
 
+    public Provider getProvider() {
+        return this.provider;
+    }
+
     public DateRange getDateRange() {
         return this.range;
     }
